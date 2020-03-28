ce15dff3c487b97ae8eb9145c6956c609f858ed0
Conor Canton <conorstreete@gmail.com>
2020-03-28 17:20:08 +0000

    profile pic now updated

--- src/app/my-profile/my-profile.component.ts
+++ src/app/my-profile/my-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-my-profile',
@@ -6,11 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-profile.component.scss']
 })
 export class MyProfileComponent implements OnInit {
+  user =null;
+  email = null;
+  avatarUrl:string;
 
   interests = ["Reading", "Gardening", "Painting", "Baking"];
-  constructor() { }
+  
+  constructor(public afAuth: AngularFireAuth){
+
+  }
+
+  getUserEmail(){
+    this.user = firebase.auth().currentUser;
+
+    if(this.user != null){
+      this.email = this.user.email;
+      console.log(this.email);
+    }
+  }
+  
+  setProfilePicture(){
+    this.getUserEmail();
+    var picLocation = "profilePics/"  + this.email;
+    console.log(picLocation);
+    var picRef = firebase.storage().ref(picLocation);
+    
+    picRef.getDownloadURL().then(picUrl => {
+      this.avatarUrl = picUrl;
+    });
+  }
+
+  
+  
 
   ngOnInit(): void {
+    this.setProfilePicture();
   }
 
 }
