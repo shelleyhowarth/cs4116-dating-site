abde4ebb053a53abafcc1693499c759cfb2d15b7
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2020-03-31 20:40:59 +0100

    filtering works with singular values

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -49,13 +49,36 @@ export class SearchComponent implements OnInit {
     "Wicklow"
   ];
   form: FormGroup;
-  filter = { gender: '', ageRange: [], location: [], interests: [] };
+  filter = { gender: '', ageRange: '', county: '', interests: '' };
   users: Array<User> = [];
-  searchArray: Array<User> = [];
+  searchArray = [];
+  noResults = true;
+
   constructor(private fb: FormBuilder, private firestore: AngularFirestore, private usersService: UsersService) { }
 
   ngOnInit(): void {
-
+    const snapshot = this.firestore.collection('Users').get();
+    snapshot.subscribe(snap => {
+       snap.forEach(doc => {
+          let object = new User();
+          object.firstName = doc.data().firstName;
+          object.lastName = doc.data().lastName;
+          object.age = doc.data().age;
+          object.description = doc.data().description;
+          object.gender = doc.data().gender;
+          object.email = doc.data().email;
+          object.favoriteSong = doc.data().favoriteSong;
+          object.favoriteMovie = doc.data().favoriteMovie;
+          object.county =  doc.data().county;
+          object.drinker = doc.data().drinker;
+          object.maritalStatus = doc.data().maritalStatus;
+          object.occupation = doc.data().occupation;
+          object.smoker = doc.data().smoker;
+          object.interests = doc.data().interests;
+          object.uid = doc.data().uid;
+          this.users.push(object);
+        });
+       });
   }
 
   getName(user) {
@@ -67,12 +90,37 @@ export class SearchComponent implements OnInit {
   }
 
   search() {
-    this.usersService.getUsers(this.users);
+    this.noResults = true;
+    this.searchArray = [];
     console.log(this.filter);
-    this.users = this.users.filter(user => {
-      user.gender === this.filter.gender;
-      })
-    console.log(this.users);
+      this.users.forEach(user => {
+          if(user.gender === this.filter.gender || this.filter.gender === '')
+          {
+            if(user.county === this.filter.county || this.filter.county === '')
+            {
+              if(user.interests.find(interest => interest == this.filter.interests) || this.filter.interests === '') {
+                if(this.getAgeRange(user.age) || this.filter.ageRange === '') {
+                  
+                  this.searchArray.push(user);
+                  if(this.searchArray.length > 0) {
+                    this.noResults = false;
+                  }
+                  else if(this.searchArray.length == 0) {
+                    this.noResults = true;
+                } 
+              }
+            }
+          }
+        }
+      });
+    console.log(this.searchArray);
    }
+
+   getAgeRange(userAge) {
+    let firstNumber = +this.filter.ageRange.substring(0, this.filter.ageRange.indexOf("-"));
+    let secondNumber = +this.filter.ageRange.substring(this.filter.ageRange.indexOf("-") + 1, this.filter.ageRange.length);
+    return (userAge >= firstNumber && userAge <= secondNumber);
+    }
+
 
 }
