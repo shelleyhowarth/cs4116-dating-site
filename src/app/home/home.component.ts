dc84b663abc3cd8fa00817492b7c1063df00a80e
aaron long legs <longaaron331@gmail.com>
2020-04-04 23:33:14 +0100

    minor changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { ChatMessage } from '../model/chat-message.model';
-import { User } from '../model/user.model';
-import { ChatService } from '../services/chat.service';
 import * as firebase from "firebase";
-
+import { User } from '../model/user.model';
+import { notification } from '../model/notification.model';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-home',
@@ -13,97 +12,76 @@ import * as firebase from "firebase";
 })
 export class HomeComponent implements OnInit {
 
-  message: string;
-  users:  Array<User> = [];
+  userId = firebase.auth().currentUser.uid
+  notifications: Array<notification> = [];
+  users: Array<User> = [];
+  connectedUserIds: Array<String> = [];
+  searchId: Array<String> = [];
+  connectedId;
   chatId;
-  chatId1;
-  chatId2;
-  recieverUid;
-  currentUser = firebase.auth().currentUser;
-  senderUid = this.currentUser.uid;
-  userSelected: boolean = false;
   reciever;
-  messages: Array<ChatMessage>
-
+  senderUid = this.userId;
+  receiverId;
+  
   constructor(private db: AngularFirestore,private chat: ChatService) { }
 
   ngOnInit(): void {
+    this.getNotifications();
     this.users = this.chat.displayUsers();
   }
 
-  send(){
-    this.sendMessage(this.message);
-    this.message = '';
-    this.getMessages();
+  
+  checkForUser(secondUser) {
+    this.reciever = secondUser.uid;
   }
 
-  sendMessage(message){
-    console.log(this.currentUser.uid);
-    const timestamp = new Date().toLocaleString()
-    const email = this.currentUser.email;
-    //this.chatMessages = this.getMessages();
-    this.db.collection('chats').doc(this.chatId).collection('messages').add({
-      message: message,
-      timeSent: timestamp,
-      sender: this.senderUid,
-      reciever: this.recieverUid
+  getNotifications() {
+  
+    var ref = this.db.collection("notifications").get();
+    ref.subscribe(snap => {
+      snap.forEach(doc => {
+        let object = new notification;
+        var data = doc.data();
+        console.log("user id = " + this.userId + "doc id" + doc.id);
+        if (doc.id.includes(this.userId) && !(doc.data().seen)) {
+          object.timeSent = data.date;
+          object.senderName = 
+          object.notification = data.notification;
+          object.seen = data.seen;
+          this.notifications.push(object);
+        }
+      });
     });
-    console.log('Called sendMessage()');
   }
 
+  submit() {
+    var docRef = this.db.collection("notification").doc(this.userId).get();
+    docRef.subscribe(doc => {
+      this.chatId = doc.data().receiver;
+      this.receiverId = doc.data().receiver;
+      this.updateDb();
+    });
+
+  }
 
   getName(user) {
     return user.firstName;
   }
 
-
   onClick(user) {
     this.checkForUser(user);
-    this.userSelected = true;
+    
     this.users = [];
     console.log(user.uid);
     this.reciever = user;
   }
 
-  checkForUser(secondUser) {
-    this.recieverUid = secondUser.uid;
-    this.chatId1 = this.senderUid + this.recieverUid;
-    this.chatId2 = this.recieverUid + this.senderUid;
-    console.log("Chat 1" + this.chatId1);
-    console.log("Chat 2" + this.chatId2);
+  updateDb() {
+    const time = new Date().toLocaleString();
 
-    const snapshot = this.db.collection('chats').get();
-    snapshot.subscribe(snap => {
-       snap.forEach(doc => {
-         if(doc.id === this.chatId1) {
-           this.chatId = this.chatId1;
-           this.messages = this.getMessages();
-         }
-         else if(doc.id === this.chatId2) {
-           this.chatId = this.chatId2;
-           this.messages = this.getMessages();
-         }
-       });
-       console.log("chatId " + this.chatId);
-      });
-  }
-
-
-  getMessages(){
-    this.messages = [];
-    const snapshot = this.db.collection('chats').doc(this.chatId).collection('messages').get();
-    snapshot.subscribe(snap => {
-       snap.forEach(doc => {
-        var data = doc.data();
-        console.log(data);
-         let object = new ChatMessage;
-         object.userUid = data.sender;
-         object.message = data.message;
-         object.timeSent = data.timeSent;
-         console.log(object);
-         this.messages.push(object);
-       });
-      });
-      return this.messages;
+    var ref2 = this.db.collection("notifications").doc(this.userId);
+    ref2.update({
+      seen: true
+    });
   }
 }
