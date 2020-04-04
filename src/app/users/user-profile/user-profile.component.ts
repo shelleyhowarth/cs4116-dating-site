57f30e3cc16bcf54fab449ff7ce1a70e339ad73b
shelleyhowarth <37180741+shelleyhowarth@users.noreply.github.com>
2020-04-04 22:23:28 +0100

    added connection pending and message buttons to other user profiles

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { User } from 'src/app/model/user.model';
 import * as firebase from 'firebase';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,24 +11,32 @@ import * as firebase from 'firebase';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
-  id;
+  otherUserId;
   currentId = firebase.auth().currentUser.uid;
   currentUser;
   user: User;
   avatarUrl;
   noConnection = false;
+  connectionPending = false;
+  connectionAccepted = false;
   constructor(private route: ActivatedRoute, private router: Router, private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.id = this.router.url.substring(this.router.url.indexOf("=") + 1, this.router.url.length);
+    this.otherUserId = this.router.url.substring(this.router.url.indexOf("=") + 1, this.router.url.length);
 
-    var docId = this.currentId + this.id;
+    var docId = this.currentId + this.otherUserId;
     var ref = this.db.collection("Connections").doc(docId).get();
     console.log(ref);
     ref.subscribe(doc => {
       if (doc.exists) {
         console.log("Connection Exists")
         this.noConnection = false;
+        if(doc.data().accepted === true) {
+          this.connectionAccepted = true;
+        }
+        else {
+          this.connectionPending = true;
+        }
       }
       else {
         console.log("no connection")
@@ -35,7 +44,7 @@ export class UserProfileComponent implements OnInit {
       }
     });
 
-    this.db.collection('Users').doc(this.id).get().subscribe( doc => {
+    this.db.collection('Users').doc(this.otherUserId).get().subscribe( doc => {
         this.user = {  
           firstName: doc.data().firstName,
           lastName: doc.data().lastName,
@@ -89,28 +98,32 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
-  submit() {
+  sendConnectRequest() {
     const time = new Date().toLocaleString();
-    var docId = this.currentId + this.id;
+    var docId = this.currentId + this.otherUserId;
 
     var ref = this.db.collection("Connections").doc(docId);
     ref.set({
       userId1: this.currentId,
-      userId2: this.id,
+      userId2: this.otherUserId,
       date: time,
       accepted: false
     });
     console.log(ref);
     window.alert("Your connection request has been sent to " + this.user.firstName);
 
-    var ref2 = this.db.collection("notifications").doc(this.id);
+    var ref2 = this.db.collection("notifications").doc(this.otherUserId);
     ref2.set({
       date: time,
       notification: (this.currentUser.firstName + " wants to connect with you."),
       seen: false,
       connectionId: docId,
       sender: this.currentId,
-      receiver: this.id
+      receiver: this.otherUserId
     });
   }
 }
+
+  Message() {
+    this.rou
+  }
